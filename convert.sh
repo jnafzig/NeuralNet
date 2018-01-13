6efd5b517447ca7c0620bf88ac0b5a571288a3b3
Jonathan <jnafzig@purdue.edu>
2018-01-13 16:14:36 -0500

    added simple website with video

--- convert.sh
+++ convert.sh
@@ -2,6 +2,7 @@ ffmpeg -y -r 60 -f image2 -s 1920x1080 \
     -pattern_type glob -i 'video/*.png' \
     -vcodec libx264 -crf 25  -pix_fmt yuv420p \
     'docs/output.mp4'
-ffmpeg -y -i 'docs/output.mp4' \
+ffmpeg -y -r 60 -f image2 -s 1920x1080 \
+    -pattern_type glob -i 'video/*.png' \
     -vcodec libvpx -acodec libvorbis \
     'docs/output.webm'
